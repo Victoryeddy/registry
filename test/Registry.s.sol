c0fd61da7f4a31f113cdc9237311824366e9450c
Victory Ndu <victor5ndu@gmail.com>
2024-01-13 12:24:24 +0100

    Init registry smart contract

---
+++ test/Registry.s.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Registry} from "../src/Registry.sol";
+
+contract RegistryTest is Test {
+    Registry registry;
+    address alice;
+
+    function setUp() public {
+        alice = makeAddr("alice");
+        
+        registry = new Registry();
+    }
+
+    function test_register() public {
+        uint256 amountToPay = registry.PRICE();
+        
+        vm.deal(alice, amountToPay);
+        vm.startPrank(alice);
+
+        uint256 aliceBalanceBefore = address(alice).balance;
+        console2.log(aliceBalanceBefore);
+
+        registry.register{value: amountToPay}();
+
+        uint256 aliceBalanceAfter = address(alice).balance;
+        console2.log(aliceBalanceAfter);
+
+        bool isTrue = registry.isRegistered(alice);
+
+        console2.log(isTrue);
+        
+        assertTrue(registry.isRegistered(alice), "Did not register user");
+        assertEq(address(registry).balance, registry.PRICE(), "Unexpected registry balance");
+        assertEq(aliceBalanceAfter, aliceBalanceBefore - registry.PRICE(), "Unexpected user balance");
+    }
+
+    /** Code your fuzz test here */
+
+    function testUsersCanRegister(uint256 amountToPay) public {
+        vm.assume(amountToPay >= 1 ether);
+
+         vm.deal(alice, amountToPay);
+        vm.startPrank(alice);
+
+        uint256 aliceBalanceBefore = address(alice).balance;
+        console2.log(aliceBalanceBefore);
+
+        registry.register{value: amountToPay}();
+
+        uint256 aliceBalanceAfter = address(alice).balance;
+        console2.log(aliceBalanceAfter);
+
+        bool isTrue = registry.isRegistered(alice);
+
+        console2.log(isTrue);
+    }
+}
